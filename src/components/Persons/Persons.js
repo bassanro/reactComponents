807f8fecbd4675ece94049ec05f8f5d9d561fbc4
Roshan Bassan <roshan.bassan@gmai.com>
2019-11-21 17:02:10 +1100

    Class State Management

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,5 +1,5 @@
 // functional component
-import React from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 
 // Functional components don't have state and are not inhertited form Components.
@@ -7,19 +7,42 @@ import Person from "./Person/Person";
 
 // Presentational/class based components have state and are inherited from Components.
 
-const persons = (props) => {
-  console.log("Persons.js Rendering....");
-  return props.persons.map((person, index) => {
-    return (
-      <Person
-        name={person.name}
-        age={person.age}
-        click={() => props.clicked(index)}
-        key={person.id}
-        changed={(event) => props.changed(event, person.id)}
-      />
-    );
-  });
-};
+class Persons extends Component {
+  // static getDerivedStateFromProps(pros, state) {
+  //   console.log("Persons.js: getDerivedStateFromProps ");
+  //   return state;
+  // }
 
-export default persons;
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("Persons.js: shouldComponentUpdate");
+    // Return true if React should continue updating.
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("Persons.js: getSnapshotBeforeUpdate");
+    return { message: "Snapshot!" };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("Persons.js: Components did update");
+    console.log(snapshot);
+  }
+
+  render() {
+    console.log("Persons.js Rendering....");
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          name={person.name}
+          age={person.age}
+          click={() => this.props.clicked(index)}
+          key={person.id}
+          changed={(event) => this.props.changed(event, person.id)}
+        />
+      );
+    });
+  }
+}
+
+export default Persons;
